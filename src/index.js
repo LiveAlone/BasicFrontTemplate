cb6bf5f53f48eb43963a30227a1f2a1d7ec40a61
qijunyao <qijunyao@xiaohongshu.com>
2019-07-28 01:37:34 +0800

    init all

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function component() {
     const element = document.createElement('div');
 
-    element.innerHTML = 'hello world yaoqijun';
+    element.innerHTML = 'hello world yaoqijun hahahah';
 
     console.log('test all content');
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     output:{
         path: DIST_PATH,
-        filename: '[name].js',
+        filename: 'main.js',
     },
     resolve:{
         alias:{
